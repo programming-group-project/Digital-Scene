00db7f0f1aa4642b83c0000482c5511dfb134c70
SageMoon4702 <33138040+SageMoon4702@users.noreply.github.com>
2019-03-07 11:25:27 -0500

    added easy functions

--- scene.py
+++ scene.py
@@ -1,2 +1,34 @@
 import turtle
 draw = turtle.Turtle()
+def forward(dist):
+    draw.forward(dist)
+def backward(dist):
+    draw.backward(dist)
+def left(dist):
+    draw.left(dist)
+def right(dist):
+    draw.right(dist)
+def penup():
+    draw.penup()
+def pendown():
+    draw.pendown()
+def simple_circle(rad):
+    draw.circle(rad)
+def circlenostep(rad,ext):
+    draw.circle(rad,ext)
+def circle(rad,ext,step):
+    draw.circle(rad,ext,step)
+def setposition(x, y):
+    draw.setposition(x, y)
+def speed(sped):
+    draw.speed(sped)
+def color(color):
+    draw.color(color)
+def pensize(size):
+    draw.pensize(size)
+def begin_fill():
+    draw.begin_fill()
+def end_fill():
+    draw.end_fill()
+def clear():
+    draw.clear()
