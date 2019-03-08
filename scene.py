cac7aa7c4042244e3d040676e7b77465b725d6bc
Gabe-B <gabeblack3@gmail.com>
2019-03-08 12:19:54 -0500

    added loop

--- scene.py
+++ scene.py
@@ -242,6 +242,10 @@ def draw_scene():
     draw_sky(sky_height)
     draw_water(water_height)
     draw_sun(sun_size)
+    """
+    for i in range (canvas_height):
+        draw_sun(i)
+    """
     draw_buildings()
     inf_Circle()
 
